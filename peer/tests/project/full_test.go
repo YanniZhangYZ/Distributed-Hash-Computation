edd82e1d08067bbefcfb87bfcef124b7f7427bb4
qiyuand <akaqyd@gmail.com>
2023-01-13 17:19:35 +0100

    Simple full test of the project passed.

--- peer/tests/project/full_test.go
+++ peer/tests/project/full_test.go
@@ -26,23 +26,38 @@ func Test_Simple_Submit_Execute(t *testing.T) {
 			z.WithChordFixFingerInterval(time.Millisecond*200))
 	}
 
-	// Create two nodes and let them join the blockchain
+	// Create three nodes and let them join the blockchain
+
+	// Node 1
 	node1 := newNode()
 	defer node1.Stop()
 	err := node1.JoinBlockchain(10, time.Second*600)
 	require.NoError(t, err)
 
+	// Node 2
 	node2 := newNode()
 	defer node2.Stop()
-
 	node1.AddPeer(node2.GetAddr())
 	node2.AddPeer(node1.GetAddr())
-
+	err = node1.JoinChord(node2.GetAddr())
+	require.NoError(t, err)
 	err = node2.JoinBlockchain(10, time.Second*600)
 	require.NoError(t, err)
 
+	// Node 3
+	node3 := newNode()
+	defer node3.Stop()
+	node1.AddPeer(node3.GetAddr())
+	node2.AddPeer(node3.GetAddr())
+	node3.AddPeer(node1.GetAddr())
+	node3.AddPeer(node2.GetAddr())
 	err = node1.JoinChord(node2.GetAddr())
 	require.NoError(t, err)
+	err = node3.JoinBlockchain(10, time.Second*600)
+	require.NoError(t, err)
+
+	// Wait for dictionary construction
+	time.Sleep(time.Second * 10)
 
 	// Node1 submits a request
 	// Password is apple
@@ -53,25 +68,44 @@ func Test_Simple_Submit_Execute(t *testing.T) {
 	require.NoError(t, err)
 
 	// Wait for the node to crack the password and earn the reward
-	time.Sleep(time.Second * 120)
+	password := ""
+	for {
+		password = node1.PasswordReceiveResult(hashStr, saltStr)
+		if password != "" {
+			break
+		}
+		time.Sleep(time.Second * 5)
+	}
+	require.Equal(t, "apple", password)
 
 	// Print the blockchain of each miner
 	fmt.Fprint(os.Stdout, node1.GetChain().PrintChain())
 	fmt.Fprint(os.Stdout, node2.GetChain().PrintChain())
+	fmt.Fprint(os.Stdout, node3.GetChain().PrintChain())
 
 	// Check the blockchain
-	require.Equal(t, node1.GetChain().GetTransactionCount(), 4)
-	require.Equal(t, node2.GetChain().GetTransactionCount(), 4)
+	require.Equal(t, 5, node1.GetChain().GetTransactionCount())
+	require.Equal(t, 5, node2.GetChain().GetTransactionCount())
+	require.Equal(t, 5, node3.GetChain().GetTransactionCount())
+
 	require.Equal(t, node1.GetChain().GetBlockCount(), node2.GetChain().GetBlockCount())
-	require.Equal(t, node1.GetChain().GetLastBlock().State.Len(), 3)
-	require.Equal(t, node2.GetChain().GetLastBlock().State.Len(), 3)
+	require.Equal(t, node1.GetChain().GetBlockCount(), node3.GetChain().GetBlockCount())
+
+	require.Equal(t, 4, node1.GetChain().GetLastBlock().State.Len())
+	require.Equal(t, 4, node2.GetChain().GetLastBlock().State.Len())
+	require.Equal(t, 4, node3.GetChain().GetLastBlock().State.Len())
+
 	require.Equal(t, node1.GetChain().GetLastBlock().BlockHash, node2.GetChain().GetLastBlock().BlockHash)
+	require.Equal(t, node1.GetChain().GetLastBlock().BlockHash, node3.GetChain().GetLastBlock().BlockHash)
+
+	require.NoError(t, node1.GetChain().ValidateChain())
+	require.NoError(t, node2.GetChain().ValidateChain())
+	require.NoError(t, node3.GetChain().ValidateChain())
 
 	// The contract account should transfer node1's deposit to node2
 	// Check the balance
-	require.EqualValues(t, node1.GetBalance(), 7)
-	require.EqualValues(t, node2.GetBalance(), 13)
+	require.EqualValues(t, 30, node1.GetBalance()+node2.GetBalance()+node3.GetBalance())
 	contractState, _ := node1.GetChain().GetLastBlock().State.Get("1_1")
-	require.EqualValues(t, contractState.Balance, 0)
+	require.EqualValues(t, 0, contractState.Balance)
 
 }
