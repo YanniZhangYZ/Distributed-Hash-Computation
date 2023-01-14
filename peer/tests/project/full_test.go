a6da385e36b09a70a95c305bd8cdf2ea885e4656
YanniZhangYZ <yanni.zhang.github@outlook.com>
2023-01-14 01:05:40 +0100

    Add one full test with 3 nodes, 2-bytes salt, and 2 contract. One node proposed twice.

--- peer/tests/project/full_test.go
+++ peer/tests/project/full_test.go
@@ -110,3 +110,122 @@ func Test_Simple_Submit_Execute(t *testing.T) {
 	require.EqualValues(t, 0, contractState.Balance)
 
 }
+
+func Test_Full_3Nodes_2BytesSalt_2Contracts(t *testing.T) {
+	transp := channelFac()
+
+	newNode := func() z.TestNode {
+		return z.NewTestNode(t, peerFac, transp, "127.0.0.1:0",
+			z.WithBlockchainBlockTimeout(time.Second*3),
+			z.WithBlockchainDifficulty(2),
+			z.WithBlockchainBlockSize(2),
+			z.WithHeartbeat(time.Second*1),
+			z.WithAntiEntropy(time.Second*1),
+			z.WithChordBytes(2), // correspond to salt length
+			z.WithChordStabilizeInterval(time.Millisecond*200),
+			z.WithChordFixFingerInterval(time.Millisecond*200))
+	}
+
+	// Create three nodes and let them join the blockchain
+
+	// Node 1
+	node1 := newNode()
+	defer node1.Stop()
+	err := node1.JoinBlockchain(10, time.Second*600)
+	require.NoError(t, err)
+
+	// Node 2
+	node2 := newNode()
+	defer node2.Stop()
+	node1.AddPeer(node2.GetAddr())
+	node2.AddPeer(node1.GetAddr())
+	err = node2.JoinChord(node1.GetAddr())
+	require.NoError(t, err)
+	err = node2.JoinBlockchain(10, time.Second*600)
+	require.NoError(t, err)
+
+	// Node 3
+	node3 := newNode()
+	defer node3.Stop()
+	node1.AddPeer(node3.GetAddr())
+	node2.AddPeer(node3.GetAddr())
+	node3.AddPeer(node1.GetAddr())
+	node3.AddPeer(node2.GetAddr())
+	err = node3.JoinChord(node2.GetAddr())
+	require.NoError(t, err)
+	err = node3.JoinBlockchain(10, time.Second*600)
+	require.NoError(t, err)
+
+	// Wait for dictionary construction
+	time.Sleep(time.Second * 5) // neccessary to compute dictionary
+
+	// Password is apple
+	hashStr := "6ad18f940ffbd30454e3c2ecf6178c6492deb33cd2fa142dad3b411762a57860"
+	saltStr := "003c"
+
+	err = node1.PasswordSubmitRequest(hashStr, saltStr, 3, time.Second*600)
+	require.NoError(t, err)
+
+	// Wait for the node to crack the password and earn the reward
+	password := ""
+	for {
+		password = node1.PasswordReceiveResult(hashStr, saltStr)
+		if password != "" {
+			break
+		}
+		time.Sleep(time.Second * 5)
+	}
+	require.Equal(t, "apple", password)
+
+	// Password is egg
+	hashStr2 := "f857023981c0a3e223a45d37e129c6a3ddbbfe944075895243f72e83354e1008"
+	saltStr2 := "002e"
+
+	err = node1.PasswordSubmitRequest(hashStr2, saltStr2, 5, time.Second*600)
+	require.NoError(t, err)
+
+	// Wait for the node to crack the password and earn the reward
+	password2 := ""
+	for {
+		password2 = node1.PasswordReceiveResult(hashStr2, saltStr2)
+		if password2 != "" {
+			break
+		}
+		time.Sleep(time.Second * 5)
+	}
+	require.Equal(t, "egg", password2)
+
+	// Print the blockchain of each miner
+	fmt.Fprint(os.Stdout, node1.GetChain().PrintChain())
+	fmt.Fprint(os.Stdout, node2.GetChain().PrintChain())
+	fmt.Fprint(os.Stdout, node3.GetChain().PrintChain())
+
+	// Check the blockchain
+	require.Equal(t, 7, node1.GetChain().GetTransactionCount())
+	require.Equal(t, 7, node2.GetChain().GetTransactionCount())
+	require.Equal(t, 7, node3.GetChain().GetTransactionCount())
+
+	require.Equal(t, node1.GetChain().GetBlockCount(), node2.GetChain().GetBlockCount())
+	require.Equal(t, node1.GetChain().GetBlockCount(), node3.GetChain().GetBlockCount())
+
+	require.Equal(t, 5, node1.GetChain().GetLastBlock().State.Len())
+	require.Equal(t, 5, node2.GetChain().GetLastBlock().State.Len())
+	require.Equal(t, 5, node3.GetChain().GetLastBlock().State.Len())
+
+	require.Equal(t, node1.GetChain().GetLastBlock().BlockHash, node2.GetChain().GetLastBlock().BlockHash)
+	require.Equal(t, node1.GetChain().GetLastBlock().BlockHash, node3.GetChain().GetLastBlock().BlockHash)
+
+	require.NoError(t, node1.GetChain().ValidateChain())
+	require.NoError(t, node2.GetChain().ValidateChain())
+	require.NoError(t, node3.GetChain().ValidateChain())
+
+	// The contract account should transfer node1's deposit to node2
+	// Check the balance
+	require.EqualValues(t, 30, node1.GetBalance()+node2.GetBalance()+node3.GetBalance())
+	contractState, _ := node1.GetChain().GetLastBlock().State.Get("1_1")
+	require.EqualValues(t, 0, contractState.Balance)
+
+	contractState2, _ := node1.GetChain().GetLastBlock().State.Get("1_2")
+	require.EqualValues(t, 0, contractState2.Balance)
+
+}
